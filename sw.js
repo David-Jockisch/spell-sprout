5a2ca3cd786f5d289e9cede6b3845bf277c92613
David-Jockisch <60517510+David-Jockisch@users.noreply.github.com>
2026-09-24 16:46:37 -0500

    Add dedicated profiles tab and keep grown-ups hub

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'learning-garden-v4';
-const SHELL = ['./', './index.html', './spelling.html', './grownups.html', './parents.html', './math.html', './math-settings.html', './grammar.html', './grammar-settings.html', './common.js', './math.js', './math-core.js', './math-settings.js', './math.css', './home.css', './shared.css', './manifest.webmanifest', './icon-180.png', './icon-192.png', './icon-512.png', './correct.wav', './try-again.wav'];
+const CACHE = 'learning-garden-v6';
+const SHELL = ['./', './index.html', './spelling.html', './grownups.html', './parents.html', './profiles.html', './math.html', './math-settings.html', './grammar.html', './grammar-settings.html', './common.js', './math.js', './math-core.js', './math-settings.js', './math.css', './home.css', './shared.css', './manifest.webmanifest', './icon-180.png', './icon-192.png', './icon-512.png', './correct.wav', './try-again.wav'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()));
 });
